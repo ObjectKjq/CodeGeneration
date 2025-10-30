682489387656eb08e94a57d22481bd2bb2dfeae7
kongdefang <1208230746@qq.com>
2025-10-30 11:48:34 +0800

    前端项目初始化

--- src/main/java/com/kdf/codegeneration/CodeGenerationApplication.java
+++ src/main/java/com/kdf/codegeneration/CodeGenerationApplication.java
@@ -2,8 +2,10 @@ package com.kdf.codegeneration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true) // 使得在业务逻辑中获取当前对象的代理对象
 public class CodeGenerationApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/kdf/codegeneration/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.kdf.codegeneration.controller;
+
+import com.kdf.codegeneration.common.BaseResponse;
+import com.kdf.codegeneration.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success("ok");
+    }
+}
